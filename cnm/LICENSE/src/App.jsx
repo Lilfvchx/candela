b332dcc663109d9bcd7b19f2e0b2f713dec8ad04
Lilfvchx <emiperalta256@gmail.com>
2022-05-24 22:53:24 -0300

    añadiendo react router

--- cnm/LICENSE/src/App.jsx
+++ cnm/LICENSE/src/App.jsx
@@ -1,15 +1,30 @@
 import Title from "./components/Title";
 import Carousel from "./components/Carousel";
-import VerticalOptions from "./components/VerticalOptions"
-
+import VerticalOptions from "./components/VerticalOptions";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import AboutMe from "./components/AboutMe";
 
 function App() {
   return (
-    <>
-    <Title title="Candela Errandonea Rivarola " className="titulo"/>
-    <Carousel/>
-    <VerticalOptions/>
-    </>
+    <Router>
+      <Routes>
+
+      
+        <Route exact path="/" element={
+          <>
+          <Title title="Candela Errandonea Rivarola " className="titulo" />
+          <Carousel />
+          <VerticalOptions />
+          </>
+        }>
+        </Route>
+        <Route path="/aboutme" element={<AboutMe />}/>
+        {/* contacto />
+        obras realizadas*/}
+
+
+      </Routes>
+    </Router>
   );
 }
-export default App; 
+export default App;
